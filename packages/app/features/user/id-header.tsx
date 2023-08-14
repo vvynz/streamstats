addbef964bc96bf85f5b21b5a0aec7bd7d50fd2d
Vivian Zhang <vvynz12@gmail.com>
2023-08-14 19:38:12 -0400

    Setup UserIdHeader component

--- packages/app/features/user/id-header.tsx
+++ packages/app/features/user/id-header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Text, YStack } from '@my/ui'
+import { Avatar, Text, YStack } from '@my/ui'
 
 export function UserIdHeader() {
   return (
-    <YStack jc="center" ai="center">
+    <YStack jc="center" ai="center" space="$3">
+      <Avatar circular size="$10">
+        <Avatar.Image src="https://store.linefriends.com/cdn/shop/files/BT21BABYMLBBEANDOLLRJ_4589419378918_1200x1200_1_1_400x.jpg?v=1682362586" />
+        <Avatar.Fallback bc="#f6f986" />
+      </Avatar>
       <Text>This is the user id header</Text>
     </YStack>
   )
