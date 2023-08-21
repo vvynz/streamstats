6445f1f3eecda28b2c1010abebbff4499a3c836c
Vivian Zhang <vvynz12@gmail.com>
2023-08-21 18:50:39 -0400

    Add link to sign up for spotify account

--- packages/app/features/login/loginpage.tsx
+++ packages/app/features/login/loginpage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { Button, H1, H3, Text, XStack, YStack } from '@my/ui'
+import { Button, H1, H3, Paragraph, Separator, Text, XStack, YStack } from '@my/ui'
 import { Home } from '@tamagui/lucide-icons'
 import { useLink } from 'solito/link'
 
@@ -9,8 +9,12 @@ export function LoginPage() {
     href: '/',
   })
 
-  const spotifyLink = useLink({
-    href: 'https://accounts.spotify.com/en/login?continue=https%3A%2F%2Fopen.spotify.com%2F',
+  const spotifyLogin = useLink({
+    href: 'https://accounts.spotify.com/en/login',
+  })
+
+  const newSpotifyAccount = useLink({
+    href: 'https://www.spotify.com/en/signup',
   })
   return (
     <>
@@ -18,11 +22,18 @@ export function LoginPage() {
         <H1>stream stats</H1>
         <Button {...home} icon={Home}></Button>
       </XStack>
-      <YStack ai="center" space>
+      <YStack width="100%" maxWidth={500} ai="center" space>
         <H3>Welcome to stream stats</H3>
-        <Button bg="#00C354" borderRadius={30} {...spotifyLink}>
+        <Button bg="#00C354" borderRadius={30} {...spotifyLogin}>
           Login with Spotify
         </Button>
+        <Separator alignSelf="stretch" marginVertical={15} borderColor="#ffffff" />
+        <XStack ai="baseline">
+          <Paragraph fontWeight="700">Don't have an account?</Paragraph>
+          <Button {...newSpotifyAccount} backgroundColor="$backgroundTransparent">
+            Sign up for Spotify
+          </Button>
+        </XStack>
       </YStack>
     </>
   )
