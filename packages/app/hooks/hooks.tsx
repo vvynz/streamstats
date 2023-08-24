e674bdfff09f34c29c49de34ac07aaee0b757f33
Vivian Zhang <vvynz12@gmail.com>
2023-08-24 19:45:14 -0400

    create & implement a redirectToAuthCodeFlow function

--- packages/app/hooks/hooks.tsx
+++ packages/app/hooks/hooks.tsx
@@ -1,6 +1,18 @@
 export function redirectToAuthCodeFlow(clientId: string) {
-  const verifier = ''
-  const challenge = ''
+  const verifier = generateCodeVerifier(128)
+  const challenge = await generateCodeChallenge(verifier)
+
+  localStorage.setItem('verifier', verifier)
+
+  const params = new URLSearchParams()
+  params.append('client_id', clientId)
+  params.append('response_type', 'code')
+  params.append('redirect_uri', 'http://localhost:5173/callback')
+  params.append('scope', 'user-read-private user-read-email')
+  params.append('code_challenge_method', 'S256')
+  params.append('code_challenge', challenge)
+
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`
 }
 
 function generateCodeVerifier(length: number) {
