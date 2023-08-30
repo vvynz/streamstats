330f88d558f7103bf9ef097dfe941f83195a425d
Vivian Zhang <vvynz12@gmail.com>
2023-08-29 23:54:41 -0400

    implement the post request & store access token in localstorage:

--- packages/app/hooks/hooks.tsx
+++ packages/app/hooks/hooks.tsx
@@ -61,3 +61,32 @@ generateCodeChallenge(codeVerifier).then((codeChallenge) => {
 
 const urlParams = new URLSearchParams(window.location.search)
 let code = urlParams.get('code')
+
+let codeVeri = localStorage.getItem('code_verifier')
+let body = new URLSearchParams({
+  grant_type: 'authorization_code',
+  code: code,
+  redirect_uri: redirectUri,
+  client_id: clientId,
+  code_verifier: codeVeri,
+})
+
+const response = fetch('https://accounts.spotify.com/api/token', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: body,
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('HTTP status' + response.status)
+    }
+    return response.json()
+  })
+  .then((data) => {
+    localStorage.setItem('access_token', data.access_token)
+  })
+  .catch((error) => {
+    console.error('Error', error)
+  })
