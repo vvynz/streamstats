8d6567a42102fc7d4ea81b76649947c3471d2dd5
Vivian Zhang <vvynz12@gmail.com>
2023-10-05 23:53:06 -0400

    implement the useAuth hook

--- packages/app/features/user/useAuth.jsx
+++ packages/app/features/user/useAuth.jsx
@@ -1,3 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export function useAuth(code) {}
+export function useAuth(code) {
+  const [accessToken, setAccessToken] = useState()
+
+  useEffect(() => {
+    axios
+      .post('http://localhost:3000/login', {
+        code,
+      })
+      .then((res) => {
+        setAccessToken(res.data.accessToken)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [code])
+  console.log('from hook AT=', accessToken)
+  return accessToken
+}
