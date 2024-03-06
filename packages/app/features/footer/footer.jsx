6880f3b2a2e00d5ec1cdc7f339b763f1b9571724
Vivian Zhang <vvynz12@gmail.com>
2024-03-06 00:03:40 -0500

    footer styling update

--- packages/app/features/footer/footer.jsx
+++ packages/app/features/footer/footer.jsx
@@ -1,3 +1,10 @@
+import { Paragraph, XStack } from '@my/ui'
+
 export function Footer() {
-  return 'I AM THE FOOTER'
+  return (
+    <XStack margin={50} jc="center" space>
+      <Paragraph size="$3">© 2023 Stream Stats</Paragraph>
+      <Paragraph size="$3">Contact</Paragraph>
+    </XStack>
+  )
 }
